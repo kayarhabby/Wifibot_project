43e3749b837460f6c761b0fbc0de2a0b645602f0
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2023-11-15 09:15:34 +0100

    add main to git

---
+++ main.cpp
@@ -0,0 +1,49 @@
+#include "Wifibot.h"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // Créez un objet Order avec des valeurs initiales
+    Order order(0, 0, true);
+
+    // Créez un objet Wifibot en utilisant l'objet Order
+    Wifibot myWifibot(order);
+
+    // Démarrez le robot avec une vitesse initiale
+    myWifibot.speed_up();
+    cout << "Robot en accélération." << endl;
+    myWifibot.displayAttribut();
+
+    // Effectuez une rotation vers la gauche
+    myWifibot.turn(1);
+    cout << "Rotation vers la gauche." << endl;
+    myWifibot.displayAttribut();
+
+    // Effectuez une rotation vers la droite
+    myWifibot.turn(-1);
+    cout << "Rotation vers la droite." << endl;
+    myWifibot.displayAttribut();
+
+    // Arrêtez le robot
+    myWifibot.stop();
+    cout << "Robot arrêté." << endl;
+    myWifibot.displayAttribut();
+
+    // Effectuez une rotation horaire
+    myWifibot.rotate(1);
+    cout << "Rotation horaire." << endl;
+    myWifibot.displayAttribut();
+
+    // Effectuez une rotation antihoraire
+    myWifibot.rotate(-1);
+    cout << "Rotation antihoraire." << endl;
+    myWifibot.displayAttribut();
+
+    // Réduisez la vitesse du robot
+    myWifibot.speed_down();
+    cout << "Réduction de vitesse." << endl;
+    myWifibot.displayAttribut();
+
+    return 0;
+}
