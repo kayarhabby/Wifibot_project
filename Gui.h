ec542028939e086eebae6f111f8d55f7fda16146
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2023-11-19 19:03:43 +0100

    modified gui.h file

--- Gui.h
+++ Gui.h
@@ -9,11 +9,15 @@
 //#include <gtkmm/window.h>
 //#include <gtkmm/button.h>
 #include <iostream>
+#include "Wifibot.h"
 
 class Gui : public Gtk::Window { // La classe Gui hérite de la classe Gtk::Window
 public:
     Gui(); // Constructeur
     ~Gui(); // Destructeur
+    void configureButton(Gtk::Button& button, const std::string& filename, Gtk::Image& image);
+    void configureImage(const std::string& filename, Gtk::Image& image);
+
 protected:
 
     //Child widgets:
@@ -31,26 +35,19 @@ protected:
     Gtk::Image m_image5;
     Gtk::Image m_image6;
 
-    Glib::RefPtr<Gdk::Pixbuf> m_pixbuf;
-    Glib::RefPtr<Gdk::Pixbuf> m_pixbuf1;
-    Glib::RefPtr<Gdk::Pixbuf> m_pixbuf2;
-    Glib::RefPtr<Gdk::Pixbuf> m_pixbuf3;
-    Glib::RefPtr<Gdk::Pixbuf> m_pixbuf4;
-    Glib::RefPtr<Gdk::Pixbuf> m_pixbuf5;
-    Glib::RefPtr<Gdk::Pixbuf> m_pixbuf6;
-
-    Gtk::Button button_img1;
-    Gtk::Button button_img2;
-    Gtk::Button button_img3;
-    Gtk::Button button_img4;
-    Gtk::Button button_img5;
-    Gtk::Button button_img6;
+    Gtk::Button button_up_arrow;
+    Gtk::Button button_left_arrow;
+    Gtk::Button button_right_arrow;
+    Gtk::Button button_down_arrow;
+    Gtk::Button button_spin_left_arrow;
+    Gtk::Button button_spin_right_arrow;
     // Grille de boutons
     Gtk::Grid grid;
 private:
-    Gtk::Button m_button_1; // Déclaration de l’objet membre m_button_1 du type Gtk::Button
+    Gtk::Button m_button_stop; // Déclaration de l’objet membre m_button_1 du type Gtk::Button
     Gtk::Entry m_entry;
     Gtk::Button m_button_connexion;
+    Wifibot m_robot;
 
 };
 
