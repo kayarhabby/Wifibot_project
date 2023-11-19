38f10ffe32c413491421eb70876a2d463c4ddb07
kayarhabby <pascia-hershe.kaya-rhabby@etu.unistra.fr>
2023-11-19 19:58:00 +0100

    modified Gui.h

--- Gui.h
+++ Gui.h
@@ -23,6 +23,7 @@ protected:
     //Child widgets:
     Gtk::Frame m_Frame1;
     Gtk::Frame m_Frame2;
+    Gtk::Frame m_Frame3;
     Gtk::VBox vbox;
     Gtk::HBox hbox1;
     Gtk::HBox hbox2;
@@ -43,11 +44,13 @@ protected:
     Gtk::Button button_spin_right_arrow;
     // Grille de boutons
     Gtk::Grid grid;
+    Gtk::VScale m_speedSlider;
 private:
     Gtk::Button m_button_stop; // Déclaration de l’objet membre m_button_1 du type Gtk::Button
     Gtk::Entry m_entry;
     Gtk::Button m_button_connexion;
     Wifibot m_robot;
+
 
 };
 
